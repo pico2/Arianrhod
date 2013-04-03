ed46b1b526fb4f0d5f4b6167912c9fc34a9dda73
Asuna <Hiromi.Kaede@gmail.com>
2013-04-03 16:54:50 +0800

    .

--- TempSync/SyncToolsPatcher/sync_commit.bat
+++ TempSync/SyncToolsPatcher/sync_commit.bat
@@ -1,2 +1,2 @@
 @start "" "D:\TortoiseSVN\bin\TortoiseProc.exe" /command:commit /path:"E:\SyncPrivate\"
-@start "" "D:\TortoiseGit\bin\TortoiseGitProc.exe" /command:commit /path:"E:\Sync\"
+@start "" "D:\TortoiseGit\bin\TortoiseGitProc.exe" /command:commit /logmsg:"." /path:"E:\Sync\"
