66ecfbad1184098c78b8af55ab614d5acd2cc0c4
アリアンフロッド <Hiromi.Kaede@gmail.com>
2013-06-27 16:23:53 +0800

    .

--- Source/Hooks/EDAO/Decompiler/cleanup.bat
+++ Source/Hooks/EDAO/Decompiler/cleanup.bat
@@ -1,2 +1,2 @@
-@for /F "delims=" %%i IN ('dir/b/s "%~dp0\__pycache__"') do rd/s/q "%%i" >NUL 2>NUL
-
+@rd/s/q __pycache__ >NUL 2>NUL
+@for /F "delims=" %%i in ('dir/b/s "%~dp0\__pycache__"') do rd/s/q "%%i" >NUL 2>NUL
