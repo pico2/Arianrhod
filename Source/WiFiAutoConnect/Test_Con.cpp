3ce18ddfdf0e1265a50b70d001f02a3a1a0c9907
アリアンロード <Hiromi.Kaede@gmail.com>
2013-07-03 23:31:06 +0800

    .

--- Source/WiFiAutoConnect/Test_Con.cpp
+++ Source/WiFiAutoConnect/Test_Con.cpp
@@ -7,9 +7,10 @@
 
 #pragma comment(lib, "Wlanapi.lib")
 
-Void main2(LongPtr argc, TChar **argv)
-{
+LONG Connect()
+{    
     BOOL                            Connected;
+    WLAN_INTERFACE_STATE            State;
     ULONG                           ErrorCode, NegotiatedVersion;
     HANDLE                          Client;
     PWLAN_INTERFACE_INFO_LIST       InterfaceList;
@@ -27,51 +28,8 @@ Void main2(LongPtr argc, TChar **argv)
     if (ErrorCode == NO_ERROR)
     FOR_EACH(InterfaceInfo, InterfaceList->InterfaceInfo, InterfaceList->dwNumberOfItems)
     {
-/*
-        PrintConsoleW(L"%.*s\n", countof(InterfaceInfo->strInterfaceDescription), InterfaceInfo->strInterfaceDescription);
-
-        switch (InterfaceInfo->isState)
-        {
-            case wlan_interface_state_not_ready:
-                PrintConsoleW(L"Not ready\n");
-                break;
-
-            case wlan_interface_state_connected:
-                PrintConsoleW(L"Connected\n");
-                break;
-
-            case wlan_interface_state_ad_hoc_network_formed:
-                PrintConsoleW(L"First node in a ad hoc network\n");
-                break;
-
-            case wlan_interface_state_disconnecting:
-                PrintConsoleW(L"Disconnecting\n");
-                break;
-
-            case wlan_interface_state_disconnected:
-                PrintConsoleW(L"Not connected\n");
-                break;
-
-            case wlan_interface_state_associating:
-                PrintConsoleW(L"Attempting to associate with a network\n");
-                break;
-
-            case wlan_interface_state_discovering:
-                PrintConsoleW(L"Auto configuration is discovering settings for the network\n");
-                break;
-
-            case wlan_interface_state_authenticating:
-                PrintConsoleW(L"In process of authenticating\n");
-                break;
-
-            default:
-                PrintConsoleW(L"Unknown state %ld\n", InterfaceInfo->isState);
-                break;
-        }
-
-        PrintConsoleW(L"\n");
-*/
-        if (InterfaceInfo->isState == wlan_interface_state_connected)
+        State = InterfaceInfo->isState;
+        if (State == wlan_interface_state_connected)
             break;
 
         ErrorCode = WlanGetAvailableNetworkList(Client, &InterfaceInfo->InterfaceGuid, 0, NULL, &NetworkList);
@@ -82,8 +40,6 @@ Void main2(LongPtr argc, TChar **argv)
         {
             if (Network->strProfileName[0] == 0)
                 continue;
-
-            //PrintConsoleW(L"%s\n", Network->strProfileName);
 
             ULONG                       Access;
             PWSTR                       ProfileXml;
@@ -109,6 +65,41 @@ Void main2(LongPtr argc, TChar **argv)
 
     WlanFreeMemory(InterfaceList);
     WlanCloseHandle(Client, NULL);
+
+    if (ErrorCode == NO_ERROR && InterfaceList->dwNumberOfItems != 0)
+        return State;
+
+    return wlan_interface_state_not_ready;
+}
+
+Void main2(LongPtr argc, TChar **argv)
+{
+    ULONG_PTR Interval, MaxInterval, Step;
+
+    MaxInterval = 5000;
+    Step = 1000;
+
+    Interval = 500;
+    LOOP_FOREVER
+    {
+        LONG State;
+
+        State = Connect();
+        if (State == wlan_interface_state_connected)
+        {
+            if (Interval < MaxInterval)
+            {
+                Interval += Step;
+                Interval = ML_MIN(MaxInterval, Interval);
+            }
+        }
+        else
+        {
+            Interval = 500;
+        }
+        
+        Ps::Sleep(Interval);
+    }
 
     Ps::ExitProcess(0);
 }
