5b7139f694255735c3c9f57d5a0338c6afaf94b5
アリアンフロッド <Hiromi.Kaede@gmail.com>
2013-10-26 17:44:49 +0800

    .

---
+++ PyLibs/mlqt.py
@@ -0,0 +1,21 @@
+from ml import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class QtThread(QtCore.QThread):
+    def __init__(self, parent = None):
+        super(type(self), self).__init__(parent)
+
+        self.mutex = QMutex()
+        self.condition = QWaitCondition()
+        self.abort = False
+
+    def __del__(self):
+        self.mutex.lock()
+        self.abort = True
+        self.condition.wakeOne()
+        self.mutex.unlock()
+
+        self.wait()
