11c1286971bbc2a21c213a7184a3552cff1808eb
アリアンフロッド <Hiromi.Kaede@gmail.com>
2013-12-31 17:33:06 +0800

    .

---
+++ TempSync/Python3 Portable/qttest.py
@@ -0,0 +1,45 @@
+from ml import *
+from PyQt5.QtWidgets import QApplication, QDialog, QWidget
+import PyQt5.QtWidgets as QtWidgets
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
+
+class QuitButton(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Quit button')
+
+        quit = QtWidgets.QPushButton('Close', self)
+        quit.setGeometry(10, 10, 64, 35)
+
+        quit.clicked.connect(self.fuck)
+        quit.clicked.connect(self.fuck2)
+
+        self.centerOnScreen()
+
+    def fuck(self):
+        print('fuck')
+        #self.close()
+
+    def fuck2(self):
+        print('fuck2')
+        self.close()
+
+    def centerOnScreen (self):
+        resolution = QtWidgets.QDesktopWidget().screenGeometry()
+        self.move((resolution.width() / 2) - (self.frameSize().width() / 2),
+                  (resolution.height() / 2) - (self.frameSize().height() / 2))
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    qb = QuitButton()
+    qb.show()
+
+    return app.exec_()
+
+if __name__ == '__main__':
+    TryInvoke(main)
