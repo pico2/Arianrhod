77a43c6b73d8111eb83e5089a1b76962a31f4dae
アリアンロード <Hiromi.Kaede@gmail.com>
2014-07-22 10:46:31 +0800

    .

--- PyLibs/MyPyLibrary/Libs/IO/FileIo.py
+++ PyLibs/MyPyLibrary/Libs/IO/FileIo.py
@@ -6,6 +6,6 @@ def EnumDirectoryFiles(path, filter = '*.*'):
     for root, dirs, files in os.walk(path):
         for f in files:
             f = os.path.join(root, f)
-            fnmatch.filter(f, filter) and not os.path.isdir(f) and allfiles.append(f)
+            fnmatch.filter([f], filter) and not os.path.isdir(f) and allfiles.append(f)
 
     return allfiles
