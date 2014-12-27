a78bfb94ea9edc6785587f615a4c40d7bfd19cc6
アリアンロード <Hiromi.Kaede@gmail.com>
2014-12-27 23:02:17 +0800

    .

--- PyLibs/mlqt.py
+++ PyLibs/mlqt.py
@@ -3,6 +3,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtWebKitWidgets import *
 
 class QtThread(QtCore.QThread):
     def __init__(self, parent = None):
