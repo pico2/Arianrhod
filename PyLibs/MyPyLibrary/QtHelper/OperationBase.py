d3e81190462309039bb81ff774e642e9a26da9fb
アリアンロード <Hiromi.Kaede@gmail.com>
2015-02-20 10:33:45 +0800

    .

--- PyLibs/MyPyLibrary/QtHelper/OperationBase.py
+++ PyLibs/MyPyLibrary/QtHelper/OperationBase.py
@@ -1,13 +1,14 @@
 from ml import *
+import asyncio
 
 class OperationBase(object):
-    def __init__(self, Parent):
-        self.Parent = Parent
-        if Parent:
-            self.RegisterEventHandler   = Parent.RegisterEventHandler
-            self.UnRegisterEventHandler = Parent.UnRegisterEventHandler
-            self.PostEvent              = Parent.PostEvent
-            self.SendEvent              = Parent.SendEvent
+    def __init__(self, parent):
+        self.parent = parent
+        if parent:
+            self.RegisterEventHandler   = parent.RegisterEventHandler
+            self.UnRegisterEventHandler = parent.UnRegisterEventHandler
+            self.PostEvent              = parent.PostEvent
+            self.SendEvent              = parent.SendEvent
 
     def RegisterEventHandler(self, Event, Handler):
         raise NotImplementedError
@@ -20,3 +21,7 @@ class OperationBase(object):
 
     def SendEvent(self, Event, *args, **kwargs):
         raise NotImplementedError
+
+    @asyncio.coroutine
+    def AsyncEvent(self, *args, **kwargs):
+        pass
