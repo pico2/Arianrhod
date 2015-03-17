a3dff5e479544274ff74deb3f23f6dcd30d02ae9
アリアンロード <Hiromi.Kaede@gmail.com>
2015-03-17 18:45:39 +0800

    .

--- PyLibs/MyPyLibrary/QtHelper/Preferences.py
+++ PyLibs/MyPyLibrary/QtHelper/Preferences.py
@@ -2,58 +2,6 @@ from ml import *
 
 DefaultPreferences = '''\
 {
-    "Properties": {
-        "noTicker": false,
-        "noRescale": false,
-        "noAnimations": true,
-        "noBlending": false,
-        "noScreenSync": false,
-        "fullscreen": false,
-        "usePixmaps": false,
-        "useLoop": false,
-        "showBoundingRect": false,
-        "showFps": false,
-        "noAdapt": false,
-        "noWindowMask": true,
-        "useButtonBalls": false,
-        "useEightBitPalette": false,
-        "noTimerUpdate": false,
-        "noTickerMorph": false,
-        "adapted": false,
-        "verbose": false,
-        "pause": true
-    },
-
-    "Shortcuts": {
-        "Menu": {
-            "File": {
-                "OpenPE":   "F3",
-                "Attach":   "Ctrl+F1",
-                "Exit":     "Alt+X"
-            },
-            "Debug": {
-                "StopDebugging":    "ALT+F2"
-            }
-        },
-        "Docks": {
-            "Cpu":          "ALT+C",
-            "Memory":       "ALT+D",
-            "Registers":    "ALT+R",
-            "Stack":        "ALT+S"
-        }
-    },
-
-    "Layout": {
-        "Cpu": {
-            "Splitter": [85, 285, 905]
-        },
-        "Memory": {
-            "Splitter": [85, 660]
-        },
-        "Stack": {
-            "Splitter": [85, 171]
-        }
-    }
 }
 '''
 
@@ -79,14 +27,20 @@ def MergeDict(dict1, dict2):
             continue
 
 def CreateDefaultPreferences():
-    return json.loads(DefaultPreferences)
+    # return json.loads(DefaultPreferences)
+    return {}
 
-def LoadPreferences(Preferences):
-    default = CreateDefaultPreferences()
-    try:
-        overwrite = json.loads(open(Preferences, 'rb').read().decode('UTF8'))
-    except:
-        overwrite = dict()
+def LoadPreferences(Preferences, defaultPreferences = None):
+    default = defaultPreferences or CreateDefaultPreferences()
+
+    if isinstance(Preferences, dict):
+        overwrite = Preferences
+
+    else:
+        try:
+            overwrite = json.loads(open(Preferences, 'rb').read().decode('UTF8'))
+        except:
+            overwrite = dict()
 
     MergeDict(default, overwrite)
     return OrderedDictEx(default)
