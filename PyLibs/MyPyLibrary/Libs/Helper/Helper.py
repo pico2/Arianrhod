0cf75b95ccb171e4fc9d391cb9b9ba74ad877081
アリアンロード <Hiromi.Kaede@gmail.com>
2015-03-31 18:47:57 +0800

    .

--- PyLibs/MyPyLibrary/Libs/Helper/Helper.py
+++ PyLibs/MyPyLibrary/Libs/Helper/Helper.py
@@ -1,6 +1,32 @@
 from Libs.Misc import *
 from Libs.IO.FileIo import *
 
+def base64Encode(data, *, encoding = 'UTF8', multiline = False):
+    import base64
+
+    if isinstance(data, str):
+        data = data.encode(encoding)
+
+    data = base64.encodebytes(data)
+
+    if not multiline:
+        data = data.replace(b'\r', b'').replace(b'\n', b'')
+
+    return data.decode('ASCII')
+
+def base64Decode(data, *, encoding = None):
+    import base64
+
+    if isinstance(data, str):
+        data = data.encode('ASCII')
+
+    data = base64.decodebytes(data)
+
+    if encoding:
+        data = data.decode(encoding)
+
+    return data
+
 def HashBytes(bytes, method = 'md5'):
     import hashlib
 
