d39a60d0e40eb49394f8d5771151068f389d00f7
アリアンロード <Hiromi.Kaede@gmail.com>
2015-05-08 17:46:12 +0800

    .

--- GoLibs/ml.go
+++ GoLibs/ml.go
@@ -1,2 +1,22 @@
 package ml
 
+import (
+    "syscall"
+)
+
+func PauseConsole(text ...string) {
+    var (
+        msvcrt, _ = syscall.LoadLibrary("msvcrt.dll")
+        getch, _ = syscall.GetProcAddress(msvcrt, "_getch")
+    )
+
+    if len(text) != 0 {
+        print(text[0])
+    }
+
+    syscall.Syscall(uintptr(getch), uintptr(0), 0, 0, 0)
+}
+
+func Init() {
+    print("Init")
+}
