5fe649a14bd00ece86f166367a1ca58ae68afab4
アリアンロード <Hiromi.Kaede@gmail.com>
2015-05-11 00:16:01 +0800

    .

--- Source/test_go/test.go
+++ Source/test_go/test.go
@@ -3,8 +3,19 @@ package main
 import (
     _ "ml"
     "ml/console"
+    "log"
+    "os"
 )
 
+type Job struct {
+    Command string
+    *log.Logger
+}
+
 func main() {
+    jog := &Job{"command", log.New(os.Stderr, "[fucker] ", log.LstdFlags)}
+
+    jog.Print("fucking")
+
     console.Pause("FUCK")
 }
