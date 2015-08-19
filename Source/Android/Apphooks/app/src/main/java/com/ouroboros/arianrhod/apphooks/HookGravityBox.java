d956b3a607a4e79d8e7c24dda21ca301ef251728
アリアンロード <Hiromi.Kaede@gmail.com>
2015-08-19 18:41:03 +0800

    apphooks/HookGravityBox.java

---
+++ Source/Android/Apphooks/app/src/main/java/com/ouroboros/arianrhod/apphooks/HookGravityBox.java
@@ -0,0 +1,29 @@
+package com.ouroboros.arianrhod.apphooks;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import android.content.Context;
+import android.os.Bundle;
+
+public class HookGravityBox implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(LoadPackageParam pkg) throws Throwable {
+        XposedHelpers.findAndHookMethod("com.ceco.lollipop.gravitybox.UnlockActivity", pkg.classLoader, "checkPolicyOk", Context.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                HookLoadPackage.log("hook in");
+                param.setResult(true);
+            }
+        });
+
+        XposedHelpers.findAndHookConstructor("com.ceco.lollipop.gravitybox.GravityBoxSettings$SystemProperties", pkg.classLoader, Bundle.class, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+//                HookLoadPackage.log("hook in 2");
+                XposedHelpers.setBooleanField(param.thisObject, "uuidRegistered", true);
+            }
+        });
+    }
+}
