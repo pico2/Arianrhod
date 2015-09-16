cc00bfa7d3aef415b8aefd4cdc8db9e2a48d824e
アリアンロード <Hiromi.Kaede@gmail.com>
2015-09-16 08:07:40 +0800

    sync

--- Source/test_go/test.go
+++ Source/test_go/test.go
@@ -2,41 +2,54 @@ package main
 
 import (
     _ "ml"
-    "ml/console"
-    "path/filepath"
     . "fmt"
-    "goqml"
-    // _ "./resource"
+    // "ml/console"
+    "ml/net/pop3"
+    "net/mail"
+    "strings"
+    "io/ioutil"
 )
 
-func run() error {
-    engine := qml.NewEngine()
-    component, err := engine.LoadFile(`D:\Dev\Library\Qt\Examples\Qt-5.5\quick\controls\gallery\qml\InputPage.qml`)
-    if err != nil {
-        return err
+// func run() error {
+//     engine := qml.NewEngine()
+//     component, err := engine.LoadFile(`D:\Dev\Library\Qt\Examples\Qt-5.5\quick\controls\gallery\qml\InputPage.qml`)
+//     if err != nil {
+//         return err
+//     }
+
+//     window := component.CreateWindow(nil)
+//     window.Show()
+//     window.Wait()
+//     return nil
+// }
+
+func main2() {
+    c, err := pop3.DialTLS("pop3.sohu.com:995")
+    Println(err)
+
+    Println(c.Auth("", ""))
+    _, sizes, err := c.ListAll()
+    Println(err)
+
+    for i := 0; i != len(sizes); i++ {
+        text, err := c.Retr(i + 1)
+        Println(err)
+
+        m, err := mail.ReadMessage(strings.NewReader(text))
+        Println(err)
+        Println(m.Header)
+        body, err := ioutil.ReadAll(m.Body)
+        Println(err)
+        Println(string(body))
     }
-
-    window := component.CreateWindow(nil)
-    window.Show()
-    window.Wait()
-    return nil
-}
-
-type String2 struct {
-    string
-}
-
-func funcTakeString(str string) {
-    Println(str)
 }
 
 func main() {
-    Println(filepath.Join(`D:\Desktop\Source\Project\Private\bettor\common\match`, `../site`))
-    Println(`../site`[0] == '.')
+    main2()
     return
 
-    if err := qml.Run(run); err != nil {
-        Printf("error: %v\n", err)
-        console.Pause("done")
-    }
+    // if err := qml.Run(run); err != nil {
+    //     Printf("error: %v\n", err)
+    //     console.Pause("done")
+    // }
 }
