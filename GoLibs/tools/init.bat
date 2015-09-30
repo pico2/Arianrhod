b7b062b576337b1630c01cb63ead42b75a771f05
アリアンロード <Hiromi.Kaede@gmail.com>
2015-09-30 15:14:55 +0800

    sync

--- GoLibs/tools/init.bat
+++ GoLibs/tools/init.bat
@@ -1,5 +1,5 @@
 set GOROOT=%~dp0
-set GOPATH=%~dp0pkgs;D:\Desktop\Source\GoProject;D:\Desktop\Source\Project\Private
+set GOPATH=%~dp0pkgs;D:\Desktop\Source\GoProject;D:\Desktop\Source\Project\Private;D:\Desktop\Source\Falcom
 set PATH=%GOROOT%bin;D:\Dev\PortableGit\bin;%PATH%
 set GOGCCFLAGS="-g -O3 -fPIC -m64 -pthread --gc-sections"
 set CGO_ENABLED="1"
