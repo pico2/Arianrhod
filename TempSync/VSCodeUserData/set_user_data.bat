648d12f83f86c49351c2be0fd424bacf27b02dcd
アリアンロード <Hiromi.Kaede@gmail.com>
2016-02-22 22:14:49 +0800

    sync

---
+++ TempSync/VSCodeUserData/set_user_data.bat
@@ -0,0 +1,8 @@
+@echo off
+cd/d "%~dp0"
+
+rd/s/q "%appdata%\Code\User"
+rd/s/q "%userprofile%\.vscode\extensions"
+
+mklink /j "%appdata%\Code\User" .
+mklink /j "%userprofile%\.vscode\extensions" .\extensions
