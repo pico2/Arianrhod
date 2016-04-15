9a4aa1c935243744c275ccb1120fd64716484ba8
アリアンロード <Hiromi.Kaede@gmail.com>
2016-04-15 16:26:24 +0800

    QQ Group message revoke

--- Source/Hooks/QQ/Hooks/HookIM.cpp
+++ Source/Hooks/QQ/Hooks/HookIM.cpp
@@ -24,11 +24,31 @@ PVOID SearchIM_RemoveRevokedMsg(PVOID ImageBase)
     return Found == nullptr ? IMAGE_INVALID_VA : Found;
 }
 
+PVOID SearchIM_RemoveRevokedGroupMsg(PVOID ImageBase)
+{
+    static CHAR String[] = "tencent.im.msgrevoke.MsgInfoUserDef";
+
+    PLDR_MODULE Module;
+    PVOID Found;
+
+    SEARCH_PATTERN_DATA Str[] =
+    {
+        ADD_PATTERN_(String, sizeof(String)),
+    };
+
+    Module = FindLdrModuleByHandle(ImageBase);
+
+    Found = SearchPattern(Str, countof(Str), Module->DllBase, Module->SizeOfImage);
+
+    return Found == nullptr ? IMAGE_INVALID_VA : Found;
+}
+
 NTSTATUS HookIM(PVOID BaseAddress)
 {
     Mp::PATCH_MEMORY_DATA Function_IM[] =
     {
         Mp::MemoryPatchVa(0ull, 1, SearchIM_RemoveRevokedMsg(BaseAddress)),
+        Mp::MemoryPatchVa(0ull, 1, SearchIM_RemoveRevokedGroupMsg(BaseAddress)),
     };
 
     return Mp::PatchMemory(Function_IM, countof(Function_IM), BaseAddress);
