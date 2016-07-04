fdfd433760b2a491e3dfa15c62383687986f22f7
アリアンロード <Hiromi.Kaede@gmail.com>
2016-07-04 16:08:58 +0800

    sync

--- Source/test_go/test.go
+++ Source/test_go/test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+    "github.com/garyburd/redigo/redis"
+    "gopkg.in/redsync.v1"
     "github.com/PuerkitoBio/goquery"
     . "ml/strings"
     . "ml/trace"
@@ -39,18 +41,35 @@ func checkDuplicate() {
 }
 
 func main() {
-    for i := 0; i != 500; i++ {
-        go func() {
-            for {
-                Try(func() {
-                    h := http.NewSession()
-                    defer h.Close()
-                    h.SetProxy("localhost", 48888)
-                    h.Get("http://www.qq.com")
-                })
-            }
-        }()
+    pool := redis.NewPool(
+                func() (redis.Conn, error) {
+                    return redis.DialTimeout(
+                                "tcp",
+                                "192.168.1.2:6379",
+                                time.Second * 10,
+                                time.Second * 60,
+                                time.Second * 60,
+                            )
+                },
+                3,
+            )
+    defer pool.Close()
+
+    sync := redsync.New([]redsync.Pool{pool})
+
+    m := sync.NewMutex(
+            "get_account",
+            redsync.SetExpiry(time.Minute * 10),
+            redsync.SetDriftFactor(1),
+        )
+
+    for m.Lock() == redsync.ErrFailed {
     }
 
-    time.Sleep(time.Hour)
+    for i := 0; i != 100; i++ {
+        fmt.Println(i)
+        time.Sleep(time.Second)
+    }
+
+    m.Unlock()
 }
