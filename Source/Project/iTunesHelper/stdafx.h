6842aa460b17c8ebf4332ebb798de961ad48c539
アリアンロード <Hiromi.Kaede@gmail.com>
2016-10-12 13:54:19 +0800

    sync

--- Source/Project/iTunesHelper/stdafx.h
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define _CRT_NON_CONFORMING_SWPRINTFS
-
-#include "ml.h"
-#include "SectionProtector.h"
-#include "iTunesHelper.h"
-
-using namespace iTunesApi::AMD;
